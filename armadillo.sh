d1a5a92226d430bf996d61396b7dacb7ba996ff4
Ygor Máximo <ygormaximo@protonmail.ch>
2018-06-10 11:06:22 -0300

    Changed code structure

--- armadillo.sh
+++ armadillo.sh
@@ -15,6 +15,94 @@ fi
 set -e
 
 export PATH=$PATH:`pwd`
+srv_conf="/etc/openvpn/server.conf"
+dns_srv_conf_1=`cat $srv_conf | grep -i "dhcp-option" | cut -d " " -f4 | cut -d "\"" -f1 | sed -n 1p`
+dns_srv_conf_2=`cat $srv_conf | grep -i "dhcp-option" | cut -d " " -f4 | cut -d "\"" -f1 | sed -n 2p`
+sysctl_dir="/etc/sysctl.conf"
+all_net_interfaces=`ip addr show | cut -d " " -f2 | cut -d ":" -f1`
+net_interface=`ip route | grep default | cut -d " " -f5`
+net_addr_w_mask=`ip route | grep / | cut -d " " -f1`
+vars_dir="/etc/openvpn/easy-rsa/vars"
+
+function ask_port()
+{
+	read -p "Port number (default 1194): " port
+
+	if [ "$port" -ge 1000 -a "$port" -le 47000 ]; then
+			if [ "$port" != 1194 -a "$port" ]; then
+					sed -i "s/port 1194/port $port/g" "$srv_conf"
+			else
+				echo "This is not a valid port number! Try a number a between 1000 and 47000."
+				ask_port
+			fi
+	elif [ -z "$port"  ]; then
+		echo "Default port number it is...."
+		continue
+	else
+		echo "This is not a valid port number! Try a number a between 1000 and 47000."
+		ask_port
+	fi
+}
+
+function ask_protocol()
+{
+	echo ""
+	read -p "Protocol (default is UDP): " protocol
+
+	if [ `echo "$protocol" | sed "/([t|T][c|C][p|P])/p"` ]; then
+			sed -i "s/proto udp/;proto udp/g" "$srv_conf"
+			sed -i "s/;proto tcp/proto tcp/g" "$srv_conf"
+	elif [ `echo "$protocol" | sed "/([u|U][d|D][p|P])/p"` ]; then
+		continue
+	elif [ -z "$protocol" ]; then
+		echo "Default port protocol it is...."
+		continue
+	else
+		echo "This is not a valid protocol! Try UDP or TCP."
+		ask_protocol
+	fi
+}
+
+function ask_dns()
+{
+	echo ""
+	read -p "Set custom DNS servers? (y/N): " opt
+
+	if [[ "$opt" == "y" || "$opt" == "Y" ]]; then
+			read -p "DNS 1: " dns_1
+			read -p "DNS 2: " dns_2
+			echo ""
+			echo "Setting DNS servers...."
+
+			sed -i "s/DNS $dns_srv_conf_1/DNS $dns_1/g" "$srv_conf"
+			sed -i "s/DNS $dns_srv_conf_2/DNS $dns_2/g" "$srv_conf"
+	elif [[ "$opt" == "n" || "$opt" == "N" || "$opt" == "" ]]; then
+			continue
+	exit
+	else
+		echo "Bad character. Try again."
+		ask_dns
+	fi
+}
+
+function ask_interface()
+{
+	echo "Network interface detected: $net_interface"
+	read -p "Use it? [Y/n] " answer
+	
+	if [[ "$answer" == "n" || "$answer" == "N" ]]; then
+			echo "Showing interfaces:"
+			for interface in "$all_net_interfaces"
+			do
+				echo "$interface"
+			done
+	elif [[ "$answer" == "y" || "$answer" == "Y" || "$answer" == "" ]]; then
+			continue
+	else
+		echo "Bad character. Try again."
+		ask_interface
+	fi
+}
 
 clear
 
@@ -22,7 +110,7 @@ echo ""
 echo "Detecting system...."
 echo ""
 echo "Installing openvpn packages...."
-# installs dependencies depending on system
+
 if [ `uname` == "Linux" ]; then
 	if [ -f /etc/apt/apt.conf ]; then	
 		echo "Debian-based distro detected."
@@ -51,11 +139,6 @@ echo "OpenVPN parameters:"
 echo "Hit ENTER for defaults values"
 echo ""
 
-srv_conf="/etc/openvpn/server.conf"
-
-dns_srv_conf_1=`cat $srv_conf | grep -i "dhcp-option" | cut -d " " -f4 | cut -d "\"" -f1 | sed -n 1p`
-dns_srv_conf_2=`cat $srv_conf | grep -i "dhcp-option" | cut -d " " -f4 | cut -d "\"" -f1 | sed -n 2p`
-
 sed -i "s/;push \"redirect-gateway def1 bypass-dhcp\"/push \"redirect-gateway def1 bypass-dhcp\"/g" "$srv_conf"
 sed -i "s/;push \"dhcp-option DNS $dns_srv_conf_1\"/push \"dhcp-option DNS $dns_srv_conf_1\"/g" "$srv_conf"
 sed -i "s/;push \"dhcp-option DNS $dns_srv_conf_2\"/push \"dhcp-option DNS $dns_srv_conf_2\"/g" "$srv_conf"
@@ -67,67 +150,6 @@ echo "Setting Diffie-Hellman algorithm from 1024 bits to 2048 bits...."
 sed -i "s/dh dh1024.pem/dh dh2048.pem/g" "$srv_conf"
 echo ""
 
-ask_port()
-{
-	read -p "Port number (default 1194): " port
-
-	if [ "$port" -ge 1000 -a "$port" -le 47000 ]; then
-			if [ "$port" != 1194 -a "$port" ]; then
-					sed -i "s/port 1194/port $port/g" "$srv_conf"
-			else
-				echo "This is not a valid port number! Try a number a between 1000 and 47000."
-				ask_port
-			fi
-	elif [ -z "$port"  ]; then
-		echo "Default port number it is...."
-		continue
-	else
-		echo "This is not a valid port number! Try a number a between 1000 and 47000."
-		ask_port
-	fi
-}
-
-ask_protocol()
-{
-	echo ""
-	read -p "Protocol (default is UDP): " protocol
-
-	if [ `echo "$protocol" | sed "/([t|T][c|C][p|P])/p"` ]; then
-			sed -i "s/proto udp/;proto udp/g" "$srv_conf"
-			sed -i "s/;proto tcp/proto tcp/g" "$srv_conf"
-	elif [ `echo "$protocol" | sed "/([u|U][d|D][p|P])/p"` ]; then
-		continue
-	elif [ -z "$protocol" ]; then
-		echo "Default port protocol it is...."
-		continue
-	else
-		echo "This is not a valid protocol! Try UDP or TCP."
-		ask_protocol
-	fi
-}
-
-ask_dns()
-{
-	echo ""
-	read -p "Set custom DNS servers? (y/N): " opt
-
-	if [[ "$opt" == "y" || "$opt" == "Y" ]]; then
-			read -p "DNS 1: " dns_1
-			read -p "DNS 2: " dns_2
-			echo ""
-			echo "Setting DNS servers...."
-
-			sed -i "s/DNS $dns_srv_conf_1/DNS $dns_1/g" "$srv_conf"
-			sed -i "s/DNS $dns_srv_conf_2/DNS $dns_2/g" "$srv_conf"
-	elif [[ "$opt" == "n" || "$opt" == "N" || "$opt" == "" ]]; then
-			continue
-	exit
-	else
-		echo "Bad character. Try again."
-		ask_dns
-	fi
-}
-
 ask_port
 ask_protocol
 ask_dns
@@ -138,36 +160,11 @@ if [ `cat /proc/sys/net/ipv4/ip_forward` == "0" ]; then
 	echo 1 > /proc/sys/net/ipv4/ip_forward
 fi
 
-# HERE IS SYSCTL.CONF FILE MANIPULATION
-sysctl_dir="/etc/sysctl.conf"
+# HERE IS SYSCTL.CONF FILE MANIPULATIO
 
 sed -i "s/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g" "$sysctl_dir"
 
 # ----------------------------------------------------------------------- FIREWALL RULES
-
-all_net_interfaces=`ip addr show | cut -d " " -f2 | cut -d ":" -f1`
-net_interface=`ip route | grep default | cut -d " " -f5`
-net_addr_w_mask=`ip route | grep / | cut -d " " -f1`
-
-ask_interface()
-{
-	echo "Network interface detected: $net_interface"
-	read -p "Use it? [Y/n] " answer
-	
-	if [[ "$answer" == "n" || "$answer" == "N" ]]; then
-			echo "Showing interfaces:"
-			for interface in "$all_net_interfaces"
-			do
-				echo "$interface"
-			done
-	elif [[ "$answer" == "y" || "$answer" == "Y" || "$answer" == "" ]]; then
-			continue
-	else
-		echo "Bad character. Try again."
-		ask_interface
-	fi
-}
-
 if [ `ufw status` == "active" ]; then
 		set_rules_ufw
 elif [ `firewall-cmd --state` == "running" ]; then
@@ -179,14 +176,10 @@ fi
 # ----------------------------------------------------------------------- FIREWALL RULES END
 
 # RSA Keys generations
-
 cp -r /usr/share/easy-rsa /etc/openvpn
 mkdir /etc/openvpn/easy-rsa/keys
 
-	# HERE IS VARS FILE MANIPULATION
-
-vars_dir="/etc/openvpn/easy-rsa/vars"
-
+# HERE IS VARS FILE MANIPULATION
 echo "Type your OpenVPN server's information"
 echo ""
 echo ""
@@ -204,8 +197,7 @@ sed -i "s/Dallas/$k_city/g" "$vars_dir"
 sed -i "s/My Company Name/$k_org/g" "$vars_dir"
 sed -i "s/sammy@example.com/$k_email/g" "$vars_dir"
 sed -i "s/MyOrganizationalUnit/$k_ou/g" "$vars_dir"
-
-	# ENDS FILE MANIPULATION
+# ENDS FILE MANIPULATION
 
 openssl dhparam -out /etc/openvpn/dh2048.pem 2048
 
@@ -231,7 +223,7 @@ service openvpn start
 service openvpn status
 
 # generates the client(s) key
-generate_key()
+function generate_key()
 {
 ./build-key
 
